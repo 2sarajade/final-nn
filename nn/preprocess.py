a9ef43d8f2d0a28e480ab7096b17699ef3281f12
2sarajade <2sarajade@gmail.com>
2024-03-16 21:14:52 -0700

    preprocess functions

--- nn/preprocess.py
+++ nn/preprocess.py
@@ -20,7 +20,18 @@ def sample_seqs(seqs: List[str], labels: List[bool]) -> Tuple[List[str], List[bo
         sampled_labels: List[bool]
             List of labels for the sampled sequences
     """
-    pass
+    grp1 = [seqs[i] for i in range(len(seqs)) if labels[i]]
+    grp2 = [seqs[i] for i in range(len(seqs)) if  not labels[i]]
+
+    class_size = max(len(grp1), len(grp2))
+    select1 = list(np.random.choice(grp1, size = class_size, replace = True))
+    select2 = list(np.random.choice(grp2, size = class_size, replace = True))
+    labels = [True] * class_size + [False] * class_size
+    samps = select1 + select2
+    combined = list(zip(samps, labels))
+    np.random.shuffle(combined)
+    shuff_seq, shuff_label = zip(*combined)
+    return (shuff_seq, shuff_label)
 
 def one_hot_encode_seqs(seq_arr: List[str]) -> ArrayLike:
     """
@@ -41,4 +52,19 @@ def one_hot_encode_seqs(seq_arr: List[str]) -> ArrayLike:
                 G -> [0, 0, 0, 1]
             Then, AGA -> [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0].
     """
-    pass
+    translated = []
+    for seq in seq_arr:
+        translated.append(_one_hot_encode(seq))
+    return np.array(translated)
+
+def _one_hot_encode(seq):
+    one_hot = {'A':[1,0,0,0],
+               'T':[0,1,0,0],
+               'C':[0,0,1,0],
+               'G':[0,0,0,1]}
+
+    translated = []
+    for base in seq:
+        translated = translated + one_hot.get(base)
+    
+    return translated
--- test/test_nn.py
+++ test/test_nn.py
@@ -1,4 +1,9 @@
 # TODO: import dependencies and write unit tests below
+import pytest
+import nn.preprocess as preprocess
+#import nn.io as io
+#import nn.nn as nn
+import numpy as np
 
 def test_single_forward():
     pass
@@ -25,7 +30,29 @@ def test_mean_squared_error_backprop():
     pass
 
 def test_sample_seqs():
-    pass
+    seqs = ["AA", "CC", "AG", "GT", "TC", "CA"]
+    labels = [1,1,0,0,0,0]
+    sseqs, slabels = preprocess.sample_seqs(seqs, labels)
+
+    assert len(sseqs) == len(slabels)
+    assert np.sum(slabels) == 4
+    assert len(sseqs) == 8
+
 
 def test_one_hot_encode_seqs():
-    pass
+    """
+    A -> [1, 0, 0, 0]
+    T -> [0, 1, 0, 0]
+    C -> [0, 0, 1, 0]
+    G -> [0, 0, 0, 1]
+    """
+    seqs = ["ATCG", "AAAA"]
+    one_hot = preprocess.one_hot_encode_seqs(seqs)
+    truth = np.array([
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0]
+    ])
+
+    assert one_hot.shape[0] == 2
+    assert one_hot.shape[1] == 16
+    assert np.array_equal(one_hot, truth)
